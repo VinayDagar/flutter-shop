ee771ad03b0064e3cfcebe222678dd8367a67622
Vinay Dagar <vinay.dagar102@gmail.com>
2021-05-23 12:22:26 +0530

    added snackbar on adding product to cart

--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -44,6 +44,11 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Product added to cart!'),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
